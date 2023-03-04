393deaa2d7903339d5ece26faeb06d0abec0a32b
Jasolu <jasolu@jerryawesome.com>
2023-03-03 22:25:24 -0500

    5

--- src/main/java/com/coska/aws/DynamoDBConfig.java
+++ src/main/java/com/coska/aws/DynamoDBConfig.java
@@ -1,16 +1,18 @@
 package com.coska.aws;
 
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableDynamoDBRepositories(basePackages = "com.coska.aws")
 public class DynamoDBConfig {
 
     @Value("${aws.region}")
@@ -26,17 +28,13 @@ public class DynamoDBConfig {
     private String dynamodbSecretKey;
 
     @Bean
-    public DynamoDBMapper dynamoDBMapper() {
-        return new DynamoDBMapper(buildAmazonDynamoDB());
-    }
-
-    private AmazonDynamoDB buildAmazonDynamoDB() {
+    public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder
                 .standard()
                 .withEndpointConfiguration(
-                   new AwsClientBuilder.EndpointConfiguration(dynamodbEndpoint,awsRegion))
+                        new AwsClientBuilder.EndpointConfiguration(dynamodbEndpoint, awsRegion))
                 .withCredentials(new AWSStaticCredentialsProvider(
-                   new BasicAWSCredentials(dynamodbAccessKey,dynamodbSecretKey)))
+                        new BasicAWSCredentials(dynamodbAccessKey, dynamodbSecretKey)))
                 .build();
     }
 }
